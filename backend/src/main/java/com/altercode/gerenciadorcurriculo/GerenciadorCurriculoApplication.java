c6183c154e56f9b4a134d4389784a2fab9de034d
Henrique Santos <rickalmeida720@gmail.com>
2024-01-04 17:54:58 -0300

    Criac§ão e configuração do projeto

---
+++ backend/src/main/java/com/altercode/gerenciadorcurriculo/GerenciadorCurriculoApplication.java
@@ -0,0 +1,13 @@
+package com.altercode.gerenciadorcurriculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorCurriculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorCurriculoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/altercode/gerenciadorcurriculo/GerenciadorCurriculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altercode.gerenciadorcurriculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorCurriculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
